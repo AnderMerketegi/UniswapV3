71ca4049f73611981a8b4ee1c0b6c7288f3756e2
Ander Merketegi <andermerke@gmail.com>
2024-09-23 23:07:45 +0200

    [Feature] Create wallet.py script for with Wallet class and corresponding methods.

---
+++ wallet/wallet.py
@@ -0,0 +1,69 @@
+from web3 import Web3
+import os
+from dotenv import load_dotenv
+from utils.utils import load_config
+
+
+class Wallet:
+
+    def __init__(self):
+
+        load_dotenv()
+
+        self.name = "metamask"
+        self.provider = "polygon"
+
+        address = os.getenv("WALLET_ADDRESS")
+        private_key = os.getenv("PRIVATE_KEY")
+
+        if not private_key or not address:
+            raise ValueError("Environment variables WALLET_ADDRESS and PRIVATE_KEY must be defined in .env file")
+
+        self.address = address
+
+        # Use Polygon network as default
+        self.web3 = Web3(Web3.HTTPProvider("https://polygon-rpc.com"))
+
+        # Verify connection
+        if not self.web3.is_connected():
+            raise ConnectionError(f"Unable to connect to: {self.provider}")
+
+        print(f"Connected to: {self.provider}")
+
+    def set_provider(self, provider: str) -> None:
+        """Set wallet provider and connect"""
+
+        config = load_config()
+        try:
+            network_url = config['networks'][provider]
+        except KeyError as e:
+            raise ValueError(f"Unable to find provider in config file: {provider}") from e
+
+        try:
+            web3_instance = Web3(Web3.HTTPProvider(network_url))
+            if not web3_instance.is_connected():
+                raise ConnectionError(f"Unable to connect to: {provider}")
+
+            self.provider = provider
+            self.web3 = web3_instance
+            print(f"Connected to: {self.provider}")
+
+        except ConnectionError as e:
+            print(f"Unable to connect to provider {provider}: {e}")
+            raise
+
+        except Exception as e:
+            print(f"Unexpected error in provider configuration: {e}")
+            raise
+
+    def get_balance(self):
+        """Get wallet balance"""
+
+        try:
+            # Get balance (in wei) and convert to ether or matic
+            balance_wei = self.web3.eth.get_balance(self.address)
+            balance_eth = self.web3.from_wei(balance_wei, 'ether')
+            return balance_eth
+        except (Exception,) as e:
+            print(f"Unable to get balance: {e}")
+            return None
