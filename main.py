7bc8699c25cca8469e7c1044a903da60bd3dc957
Ander Merketegi <andermerke@gmail.com>
2024-10-19 00:15:02 +0200

    [Feature] Try add liquidity function.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from logs.logger import Logger
 from wallet.wallet import Wallet
 from uniswapV3.uniswapV3 import UniswapV3
 
+from utils.utils import load_config, load_position
+
 
 def main():
 
@@ -15,9 +17,9 @@ def main():
     balance = my_wallet.get_balance()
     logger.info(f"Wallet balance: {balance}")
 
-    # Get UniswapV3 active pools
-    uniswap_v3 = UniswapV3(my_wallet.web3, my_wallet.address)
-    uniswap_v3.get_active_pools()
+    # Add liquidity to WETH/USDT pool equivalent to 10 USD por each token
+    uniswap_v3 = UniswapV3(my_wallet.network, my_wallet.web3, my_wallet.address)
+    uniswap_v3.add_liquidity(amount_usdt=10, price_range=(0.95, 1.05))
 
 
 if __name__ == "__main__":
